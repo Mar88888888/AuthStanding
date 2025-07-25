94734a131957549ffa05ddc737ddbe65760160d2
Artem <76451013+Mar88888888@users.noreply.github.com>
2025-07-25 21:18:30 +0300

    Global throttler implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,34 @@ import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { LoggerMiddleware } from './utils/logger.middleware';
 import * as dotenv from 'dotenv';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 dotenv.config();
 
 @Module({
-  imports: [UsersModule, DatabaseModule],
+  imports: [
+    UsersModule,
+    DatabaseModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
+
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
